8f63ebcb53ecce49801ad7b0426d5a90f3334426
Adam Zielonka <root@adamzielonka.pro>
2017-12-19 15:21:13 +0100

    Memory update

--- status.js
+++ status.js
@@ -1,5 +1,6 @@
 const os = require('os');
 var express = require('express')
+var shell = require('shelljs');
 var port = 30098
 var host = 'localhost'
 
@@ -10,12 +11,16 @@ app.get('/', function (req, res) {
 })
 
 app.get('/os/memory', function (req, res) {
-  var json = {};
+  var free = shell.exec('grep ^MemFree /proc/meminfo | awk \'{print $2}\'',{silent:true}).stdout;
+  var buffers = shell.exec('grep ^Buffers /proc/meminfo | awk \'{print $2}\'',{silent:true}).stdout;
+  var cached = shell.exec('grep ^Cached /proc/meminfo | awk \'{print $2}\'',{silent:true}).stdout;
+  free = parseInt(free) + parseInt(buffers) + parseInt(cached)
+  var json = {}
   json[`total`] = Math.round((os.totalmem()/1024/1024)*100)/100 + " MB"
-  json[`free`] = Math.round((os.freemem()/1024/1024)*100)/100 + " MB"
-  json[`used`] = Math.round(((os.totalmem() - os.freemem())/1024/1024)*100)/100 + " MB"
-  json[`percent_used`] = Math.round(((os.totalmem() - os.freemem()) / os.totalmem()) * 100)
-  res.send(json);
+  json[`free`] = Math.round((free/1024)*100)/100 + " MB"
+  json[`used`] = Math.round(((os.totalmem() - free)/1024/1024)*100)/100 + " MB"
+  json[`percent_used`] = Math.round(((os.totalmem() - free*1024) / os.totalmem()) * 100)
+  res.send(json)
 })
 
 app.get('/os/cpus', function (req, res) {
