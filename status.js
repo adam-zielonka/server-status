d94b436ef4685d33457bc6a33647e8bd1cab499a
Adam Zielonka <root@adamzielonka.pro>
2017-12-24 13:32:58 +0100

    Uninstall shelljs

--- status.js
+++ status.js
@@ -1,8 +1,8 @@
 const os = require('os');
 const si = require('systeminformation');
+const { exec } = require('child_process');
 var config = require('./config');
 var express = require('express')
-var shell = require('shelljs');
 var request = require('request');
 var app = express()
 
@@ -24,64 +24,66 @@ app.get('/os/load-average', function (req, res) {
 })
 
 app.get('/os/pm2', function (req, res) {
-  var pm2 = {}
-  pm2.processes = JSON.parse(shell.exec('pm2 jlist',{silent:true}).stdout)
-  pm2.time = Date.now()
-  res.send(pm2);
+  exec('pm2 jlist', (error, stdout, stderr) => {
+    if(!error) {
+      var pm2 = {}
+      pm2.processes = JSON.parse(stdout)
+      pm2.time = Date.now()
+      res.send(pm2);
+    } else res.status(500).send(error)
+  })
 })
 
 app.get('/os/system', function (req, res) {
-  var system = {}
-  system.hostname = os.hostname()
-  system.os = shell.exec('/usr/bin/lsb_release -ds | cut -d= -f2 | tr -d \'"\'',{silent:true}).stdout.replace(/\r?\n|\r/g,'')
-  system.kernel = shell.exec('/bin/uname -r',{silent:true}).stdout.replace(/\r?\n|\r/g,'')
-  system.uptime = shell.exec('/usr/bin/cut -d. -f1 /proc/uptime',{silent:true}).stdout.replace(/\r?\n|\r/g,'')
-  system.last_boot = shell.exec('uptime -s',{silent:true}).stdout.replace(/\r?\n|\r/g,'')
-  system.current_users = shell.exec('who -u | awk \'{ print $1 }\' | wc -l',{silent:true}).stdout.replace(/\r?\n|\r/g,'')
-  system.server_date = shell.exec('/bin/date',{silent:true}).stdout.replace(/\r?\n|\r/g,'')
-  res.send(system);
+  si.osInfo().then(data => {
+    data.time = si.time()
+    res.send(data)
+  }).catch(error => res.status(500).send(error))
 })
 
 app.get('/os/vhost', function (req, res) {
-  var info = shell.exec('apache2ctl -t -D DUMP_VHOSTS',{silent:true}).stdout
-  var vhost = []
-  for (var row of info.split('\n')) {
-    if(row.match(/^\s.*port/)) {
-      var one = {}
-      var port = false;
-      var name = false;
-      for(var v of row.split(' '))
-        if(v) {
-          if(port) {
-            one.port = v
-            port = false
-          }
-          if(name) {
-            one.name = v
-            name = false
-          }
-          if(v == "port") port = true
-          if(v == "namevhost") name = true
+  exec('apache2ctl -t -D DUMP_VHOSTS', (error, stdout, stderr) => {
+    if(!error) {
+      var vhost = []
+      for (var row of stdout.split('\n')) {
+        if(row.match(/^\s.*port/)) {
+          var one = {}
+          var port = false;
+          var name = false;
+          for(var v of row.split(' '))
+            if(v) {
+              if(port) {
+                one.port = v
+                port = false
+              }
+              if(name) {
+                one.name = v
+                name = false
+              }
+              if(v == "port") port = true
+              if(v == "namevhost") name = true
+            }
+            if(one) vhost.push(one)
         }
-        if(one) vhost.push(one)
-    }
-  }
-  var auth = () => {
-    if(config.vhost && config.vhost.user) return {
-    'auth': {
-      'user': config.vhost.user,
-      'pass': config.vhost.pass,
-      'sendImmediately': false
-    }}
-    return {}
-  }
-  var count = vhost.length;
-  for(let i=0; i<vhost.length; i++) {
-    request(`http://${vhost[i].name}:${vhost[i].port}`, auth(), (error, response, body) => {
-      vhost[i].statusCode = error ? error.code : response.statusCode
-      if(--count < 1) res.send(vhost)
-    })
-  }
+      }
+      var auth = () => {
+        if(config.vhost && config.vhost.user) return {
+        'auth': {
+          'user': config.vhost.user,
+          'pass': config.vhost.pass,
+          'sendImmediately': false
+        }}
+        return {}
+      }
+      var count = vhost.length;
+      for(let i=0; i<vhost.length; i++) {
+        request(`http://${vhost[i].name}:${vhost[i].port}`, auth(), (error, response, body) => {
+          vhost[i].statusCode = error ? error.code : response.statusCode
+          if(--count < 1) res.send(vhost)
+        })
+      }
+    } else res.status(500).send(error)
+  })
 })
 
 app.listen(config.web.port, config.web.host)
