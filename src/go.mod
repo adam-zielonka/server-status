06ad4c9a3cb94a572912661ae91c1a3ab1601a83
Adam Zielonka <root@adamzielonka.pro>
2025-12-29 21:42:05 +0100

    Refactor system info retrieval and update Uptime type to uint64

--- src/go.mod
+++ src/go.mod
@@ -6,15 +6,11 @@ require github.com/shirou/gopsutil/v4 v4.25.11
 
 require (
 	github.com/ebitengine/purego v0.9.1 // indirect
-	github.com/elastic/go-sysinfo v1.15.4 // indirect
-	github.com/elastic/go-windows v1.0.2 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
-	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/tklauser/go-sysconf v0.3.16 // indirect
 	github.com/tklauser/numcpus v0.11.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.4 // indirect
 	golang.org/x/sys v0.38.0 // indirect
-	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
 )
--- src/system.go
+++ src/system.go
@@ -1,30 +1,35 @@
 package main
 
 import (
-	"github.com/elastic/go-sysinfo"
+	"fmt"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/host"
 )
 
 type SystemInfo struct {
 	Hostname string `json:"hostname"`
 	OS       string `json:"os"`
 	Kernel   string `json:"kernel"`
-	Uptime   string `json:"uptime"`
+	Uptime   uint64 `json:"uptime"`
 	CPU      string `json:"cpu"`
 }
 
 func system() SystemInfo {
 	var result SystemInfo
 
-	info, err := sysinfo.Host()
-	if err != nil {
-		return result
-	}
+	info, _ := host.Info()
+	cpuInfo, _ := cpu.Info()
+
+	osName := fmt.Sprintf("%s %s", info.Platform, info.PlatformVersion)
+	cpuName := fmt.Sprintf("%s @ %.0f Mhz", cpuInfo[0].ModelName, cpuInfo[0].Mhz)
 
 	result = SystemInfo{
-		Hostname: info.Info().Hostname,
-		OS:       info.Info().OS.Name,
-		Kernel:   info.Info().KernelVersion,
-		Uptime:   info.Info().BootTime.String(),
+		Hostname: info.Hostname,
+		OS:       osName,
+		Kernel:   info.KernelVersion,
+		Uptime:   info.Uptime,
+		CPU:      cpuName,
 	}
 
 	return result
