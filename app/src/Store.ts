2192596cede325ef139626e22a9ff11e4c4a1f09
Adam Zielonka <root@adamzielonka.pro>
2026-01-01 15:29:43 +0100

    Refactor: Implement localStorage management in Connection class and remove autoSave module

--- app/src/Store.ts
+++ app/src/Store.ts
@@ -1,6 +1,5 @@
-import { makeAutoObservable, runInAction } from 'mobx'
+import { makeAutoObservable, reaction, runInAction } from 'mobx'
 import api from './api.ts'
-import autoSave from './autoSave.ts'
 
 export class Connection {
   user = ''
@@ -8,6 +7,30 @@ export class Connection {
 
   constructor() {
     makeAutoObservable(this)
+
+    const STORAGE_NAME = 'beta_connection'
+
+    const stored = localStorage.getItem(STORAGE_NAME)
+    if (stored) {
+      try {
+        const obj = JSON.parse(stored)
+        this.user = obj.user || ''
+        this.token = obj.token || ''
+      } catch {
+        localStorage.removeItem(STORAGE_NAME)
+      }
+    }
+
+    reaction(() => this.toJSON, json => {
+      localStorage.setItem(STORAGE_NAME, json)
+    })
+  }
+
+  get toJSON() {
+    return JSON.stringify({
+      user: this.user,
+      token: this.token,
+    })
   }
 }
 
@@ -19,7 +42,6 @@ export class Store {
 
   constructor() { 
     makeAutoObservable(this)
-    autoSave(this) 
     setTimeout(() => this.loadConf())
   }
 
