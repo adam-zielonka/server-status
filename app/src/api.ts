eb76948b885711e57002820c2499f904847610b3
Adam Zielonka <root@adamzielonka.pro>
2026-01-01 14:41:01 +0100

    Refactor: Move apiFetch function into API class and streamline fetch method

--- app/src/api.ts
+++ app/src/api.ts
@@ -7,47 +7,45 @@ type ApiResponse<T = unknown> = {
   errors?: string[],
 }
 
-async function apiFetch<T>(path: string, headers: HeadersInit = {}): Promise<ApiResponse<T>> {
-  if (import.meta.env.VITE_FAKE_API) {
-    return await fakeApi(path) as ApiResponse<T>
-  }
-
-  console.log('API Fetch:', url + path, headers)
-  const fetchHeaders = new Headers(headers)
-  fetchHeaders.append('Content-Type', 'application/json')
-  const token = window.store?.connection?.token
-  if (token && !fetchHeaders.has('Authorization')) {
-    fetchHeaders.append('Authorization', `Bearer ${token}`)
-  }
-  
-  return fetch(url + path, {
-    method: 'GET',
-    headers: fetchHeaders,
-  }).then(res => res.text())
-    .then(res => {
-      try {
-        return JSON.parse(res)
-      } catch (error) {
-        if (typeof res === 'string') {
-          return { errors: [res || 'Unknown error'] }
-        }
-
-        if (error instanceof Error) {
-          return { errors: [error.message] }
-        }
-
-        return { errors: ['Unknown error'] }
-      }
-    })
-    .catch(error => ({ errors: [error.message] }))
-}
-
 class API {
-  login = async (username: string, password: string) => {
-    return apiFetch<{token: string}>('auth', { 'Authorization': 'Basic ' + btoa(username + ':' + password) })
+  fetch = async function<T>(path: string, headers: HeadersInit = {}): Promise<ApiResponse<T>> {
+    if (import.meta.env.VITE_FAKE_API) {
+      return await fakeApi(path) as ApiResponse<T>
+    }
+
+    console.log('API Fetch:', url + path, headers)
+    const fetchHeaders = new Headers(headers)
+    fetchHeaders.append('Content-Type', 'application/json')
+    const token = window.store?.connection?.token
+    if (token && !fetchHeaders.has('Authorization')) {
+      fetchHeaders.append('Authorization', `Bearer ${token}`)
+    }
+  
+    return fetch(url + path, {
+      method: 'GET',
+      headers: fetchHeaders,
+    }).then(res => res.text())
+      .then(res => {
+        try {
+          return JSON.parse(res)
+        } catch (error) {
+          if (typeof res === 'string') {
+            return { errors: [res || 'Unknown error'] }
+          }
+
+          if (error instanceof Error) {
+            return { errors: [error.message] }
+          }
+
+          return { errors: ['Unknown error'] }
+        }
+      })
+      .catch(error => ({ errors: [error.message] }))
   }
 
-  fetch = apiFetch
+  login = async (username: string, password: string) => {
+    return this.fetch<{token: string}>('auth', { 'Authorization': 'Basic ' + btoa(username + ':' + password) })
+  }
 }
 
 export default new API()
